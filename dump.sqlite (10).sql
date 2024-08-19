c32b2f48072e69bc923d03e8541ae91c6574a2d2
Jhun3611 <jhunprofissa@gmail.com>
2024-08-19 15:17:33 -0300

    Add files via upload

---
+++ dump.sqlite (10).sql
@@ -0,0 +1,26 @@
+-- TABLE
+CREATE TABLE CuriosidadesParis2024(
+  id integer primary KEY AUTOINCREMENT,
+  curiosidade text not NULL,
+  categoria text
+  );
+CREATE TABLE Curiosidades_Paris2024(
+  id integer primary KEY AUTOINCREMENT,
+  curiosidade text not NULL,
+  categoria text
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE produtos(
+  id integer PRIMARY KEY  AUTOINCREMENT,
+  nome varchar(100)NOT NULL,
+  descrição text,
+  preco decimal(10,2),
+  dataHora datetime DEFAULT CURRENT_TIMESTAMP
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
